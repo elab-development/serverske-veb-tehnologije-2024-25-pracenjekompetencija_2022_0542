6e26388473aeaefdbdb9cbce1e073fd0aa60c8f4
MihailoRadulovic <mihailoradulovic711@gmail.com>
2025-09-25 19:34:38 +0200

    create category, credentail and verification table migrations

---
+++ database/migrations/2025_09_25_204459_create_credentials_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('credentials', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('issuer_name')->nullable();
+            $table->date('issued_at')->nullable();
+            $table->date('expires_at')->nullable();
+            $table->boolean('is_verified')->default(false);
+            $table->string('external_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('credentials');
+    }
+};
